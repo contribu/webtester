fb1a2bda5e4281d3fdb95b75bb56eea372dfa117
mac2 <mac2@example.com>
2019-01-12 15:23:03 +0900

    fix bug

--- lib/html_formatter.rb
+++ lib/html_formatter.rb
@@ -4,7 +4,7 @@ require 'erb'
 
 class HtmlFormatter
   def format(results)
-    error_results = results.select { |result| result[:error] }
+    error_results = results.select { |result| !result[:errors].empty? }
 
     erb = ERB.new(File.read("#{__dir__}/html_template.html.erb"))
     erb.result(binding)
